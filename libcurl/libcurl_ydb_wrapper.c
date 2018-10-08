2d4dc82629642bf96956eb71559e2ca28414feae
Sam Habiel <sam.habiel@gmail.com>
2018-10-08 15:29:14 -0400

    Refactor to remove duplicate code

--- libcurl/libcurl_ydb_wrapper.c
+++ libcurl/libcurl_ydb_wrapper.c
@@ -126,57 +126,13 @@ gtm_status_t curl_do(int argc, gtm_string_t *output, gtm_char_t *method, gtm_cha
 
 gtm_status_t curl(int argc, gtm_string_t *output, gtm_char_t *method, gtm_char_t *URL, gtm_string_t *payload, gtm_char_t *mime, gtm_long_t timeout, gtm_string_t *output_headers, gtm_string_t *input_headers)
 { 
-  CURLcode res;
-  
   curl_init(0);
-
-  struct MemoryStruct chunk;
-
-  chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
-  chunk.size = 0;    /* no data at this point */
-
-  /* specify URL to get */
-  curl_easy_setopt(curl_handle, CURLOPT_URL, (char *)URL);
-
-  /* send all data to this function  */
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-
-  /* we pass our 'chunk' struct to the callback function */
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-
-  /* some servers don't like requests that are made without a user-agent
-     field, so we provide one */
-  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-
-  /* get it! */
-  res = curl_easy_perform(curl_handle);
-
-  /* check for errors */
-  if(res != CURLE_OK) {
-    fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-    return (gtm_status_t)-1;
-  }
-  else {
-    
-    /*
-     * Now, our chunk.memory points to a memory block that is chunk.size
-     * bytes big and contains the remote file.
-     *
-     * Do something nice with it!
-     */
-
-    output->length = chunk.size;
-    memcpy(output->address, chunk.memory, chunk.size);
-  }
-
-  free(chunk.memory);
-
+  curl_do(argc, output, method, URL, payload, mime, timeout, output_headers, input_headers);
   curl_cleanup(0);
-
   return (gtm_status_t)0;
 }
 
-int main() /* debugger routine to make sure everything still works */
+int main() /* tester routine to make sure everything still works */
 {
   size_t output_size = 32768;
   gtm_string_t output;
