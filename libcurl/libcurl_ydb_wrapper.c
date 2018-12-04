8f21216702e57c0e685445feb4a7d8b6be163261
Sam Habiel <Sam.Habiel@gmail.com>
2018-12-04 12:05:21 -0500

    Updated license to be Apache 2.0 on the source code file; wasn't clear before

--- libcurl/libcurl_ydb_wrapper.c
+++ libcurl/libcurl_ydb_wrapper.c
@@ -6,7 +6,6 @@
  *                             \___|\___/|_| \_\_____|
  *
  * Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al.
- * Copyright (C) 2018 Sam Habiel - Modifications to work on GT.M/YDB
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -19,6 +18,19 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
+ * Copyright (C) 2018 Sam Habiel - Modifications to work on GT.M/YDB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ***************************************************************************/
 
 #include <stdio.h>
